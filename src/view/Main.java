6b9d487cb66f801bce5933f4285f96dada1cd7af
pc097 <36968630+pc097@users.noreply.github.com>
2020-09-15 17:46:48 -0300

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -10,8 +10,8 @@ public class Main {
 		for (int i = 0; i < 5; i++) {
 			sapo += i+1;
 			Thread threadFrog = new ThreadFrog(sapo);
-			threadFrog.start();
 			sapo = "Sapo ";
+			threadFrog.start();
 		}
 	}
 }
